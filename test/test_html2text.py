6029a2b3136c914e5ec5a51c47d96492cacfb3e8
Ciprian Miclaus <ciprianm@gmail.com>
2017-06-15 23:36:20 +0100

    two more tests: unescape & tag_callback

--- test/test_html2text.py
+++ test/test_html2text.py
@@ -102,6 +102,31 @@ class TestHTML2Text(unittest.TestCase):
             '&lt;pre&gt;and then&lt;div&gt; &amp; other tags'
         )
 
+    def test_unescape(self):
+        self.assertEqual(
+            '<pre>and then<div> & other tags',
+            html2text.unescape('&lt;pre&gt;and then&lt;div&gt; &amp; other tags')
+        )
+
+    def _skip_certain_tags(self, h2t, tag, attrs, start):
+        if tag == 'b':
+            return True
+
+    def test_tag_callback(self):
+        h = html2text.HTML2Text()
+        h.tag_callback = self._skip_certain_tags
+        ret = h.handle(
+            'this is a <b>txt</b> and this is a'
+            ' <b class="skip">with text</b> and '
+            'some <i>italics</i> too.'
+        )
+        self.assertEqual(
+            ret,
+            'this is a txt and this is a'
+            ' with text and '
+            'some _italics_ too.\n\n'
+        )
+
 
 def generate_test(fn):
     def _test_mod(self):
