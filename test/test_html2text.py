a095b1a9da851ad84fe5e2743c0e44c310030f44
MD. Mohiuddin Ahmed <ahmed.mohi.duet@gmail.com>
2019-01-30 22:37:50 +0600

    refactoring some for travis ci conformimg PEP-8

--- test/test_html2text.py
+++ test/test_html2text.py
@@ -88,9 +88,10 @@ def get_baseline_name(fn):
 
 def get_baseline(fn):
     name = get_baseline_name(fn)
-    with codecs.open(name, mode='r', encoding='utf8',
-        errors='surrogateescape') as f:
-                out = f.read()
+    with codecs.open(
+                     name, mode='r', encoding='utf8',
+                     errors='surrogateescape') as f:
+        out = f.read()
     out = cleanup_eol(out)
     return out
 
